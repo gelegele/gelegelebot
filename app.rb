7883513a5466b59003590810e489a60c1e8e0736
ryoishibashi <ryo500209@gmail.com>
2012-05-08 14:08:03 +0900

    sass, bootstrap

--- app.rb
+++ app.rb
@@ -1,33 +1,44 @@
 # coding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'sass'
 require 'twitter'
-#require 'pp'
+require 'pp'
 
-before do
+before do	
   Twitter.configure do |config|
-    #config.proxy = 'http://proxy.gw.fujitsu.co.jp:8080'
-    config.consumer_key       = 'ubtspgyKhPYFbC3iJjpPmQ' #ENV['CONSUMER_KEY']
-    config.consumer_secret    = 'Le1qLZAEG5Gr5dgExqCm6YnsFtzIafehhZcwIS2byk' #ENV['CONSUMER_SECRET']
-    config.oauth_token        = '83754646-sJKzDBfeE4Q4cJ52oRQ6GBdY3wZLzzt98Fnv6iI2U' #ENV['OAUTH_TOKEN']
-    config.oauth_token_secret = 'sCzN4yGBTEFve5QTXC2g6t08iroKjHqSx3J45wMXXw' #ENV['OAUTH_TOKEN_SECRET']
+    config.proxy = 'http://proxy.gw.fujitsu.co.jp:8080'
+    config.consumer_key       = 'avKZ3NXholdRuw19bpt82A' #ENV['CONSUMER_KEY']
+    config.consumer_secret    = 'KQm1lw9KdevEBwRi3WsYFhKmF2VRqbsN31AxgSX8' #ENV['CONSUMER_SECRET']
+    config.oauth_token        = '573594235-LEmaFKQ8jnWfZ9TGOslEC4Bt2pOE39wBMUxtt6gA' #ENV['OAUTH_TOKEN']
+    config.oauth_token_secret = '0nzp0btLR8jvrkln96if23Xih2D4UPMSa4d2uWPTlis' #ENV['OAUTH_TOKEN_SECRET']
   end
 end
 
 get '/' do
-  'HerokuでSinatraのHello world!'
-  prof = Twitter.user("gelegelebot")
-  prof.screen_name
-  ##pp prof
-end
-
-get '/latest' do
-  Twitter.user_timeline("gelegelebot").first.text #直近のツイート
+  @user_name = Twitter.user().screen_name
+  @time_line = Twitter.user_timeline()
+  haml :index
 end
 
 get '/tweet' do
   t = Time.now
-  #Twitter.update("Heroku Sinatra Hello world! at #{t}")
+  Twitter.update("Heroku Sinatra Hello world! at #{t}")
   "tweet complete"
-  "bye!"
 end
+
+
+
+
+
+#スタイルシートをsassで定義してみた
+get '/style.css' do
+  sass :stylesheet
+end
+#sassを使っったページ
+get '/timeline' do
+  @time_line = Twitter.user_timeline()
+  haml :timeline
+end
+
