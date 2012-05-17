aa159d75e233455c5131ab5412b97cc18f9a6879
ryoishibashi <ryo500209@gmail.com>
2012-05-17 16:44:25 +0900

    remove page-header, add link to img

--- app.rb
+++ app.rb
@@ -8,8 +8,9 @@ require 'twitter'
 require 'pp'
 require 'cgi'
 
-#イベント前に実行される
-before do	
+#起動時１回だけ実行される
+configure do
+  TWITTER_URL = "http://twitter.com/"
   Twitter.configure do |config|
     config.proxy = 'http://proxy.gw.fujitsu.co.jp:8080'
     config.consumer_key       = 'avKZ3NXholdRuw19bpt82A'
@@ -17,19 +18,28 @@ before do
     config.oauth_token        = '573594235-LEmaFKQ8jnWfZ9TGOslEC4Bt2pOE39wBMUxtt6gA'
     config.oauth_token_secret = '0nzp0btLR8jvrkln96if23Xih2D4UPMSa4d2uWPTlis'
   end
+end
+
+
+#イベント前に実行される
+before do	
   @user_name = Twitter.user().screen_name
 end
+
 
 #独自のスタイルシートはsassで定義
 get '/style.css' do
   sass :stylesheet
 end
 
+
+#タイムライン
 get '/' do
   @time_line = Twitter.home_timeline()
   haml :index
 end
 
+#ツイートする
 get '/tweet' do
   t = Time.now
   res = Twitter.update("Heroku Sinatra Hello world! at #{t}")
@@ -37,11 +47,21 @@ get '/tweet' do
   redirect '/'
 end
 
+
+#
+get '/user' do
+  pp Twitter.user()
+  Twitter.user().screen_name
+end
+
+
+#検索
 get '/search' do
   @raw_keyword = params[:keyword]
   if @raw_keyword && !@raw_keyword.empty? then
     @sanitized_keyword = CGI.escapeHTML(@raw_keyword) #サニタイズ
     @search_results = Twitter.search(@sanitized_keyword)
+    pp @search_results[0]
   end
   haml :search
 end
